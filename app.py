c7306aa171decf90d9997e2f8d60dde7a3485c4f
kyobum-yeo <kyobumy@gmail.com>
2026-05-16 18:21:26 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    version = os.getenv("APP_VERSION", "v1")
+    return f"<h1>Hello from CI/CD! 🚀</h1><p>Version: {version}</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
